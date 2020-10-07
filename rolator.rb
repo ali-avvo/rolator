72edc49d010ae02c33a002e8076d44ceed9207a3
Ali Rizvi <arizvi@avvo.com>
2020-10-07 11:16:58 -0700

    printing names of the channel members

--- rolator.rb
+++ rolator.rb
@@ -5,8 +5,21 @@ class Rolator < SlackRubyBot::Bot
   command 'honk' do |client, data, match|
     #client.say(text: 'hello', channel: data.channel)
     slacker = Slack::Web::Client.new
-    slacker.chat_postMessage(channel: '#rolator_test', text: "Time is #{Time.now}", as_user: true)
+    slacker.chat_postMessage(channel: data.channel, text: "Time is #{Time.now}", as_user: true)
   end
+
+  command 'roles' do |client, data, match|
+    slacker = Slack::Web::Client.new
+    message = "Look who is here: "
+    #require 'pry';binding.pry
+    members = slacker.conversations_members(channel: data.channel).members
+    members.each do |member|
+      message += slacker.users_info(user: member).user.real_name
+      message += ", "
+    end
+    slacker.chat_postMessage(channel: data.channel, text: message, as_user: true)
+  end
+
 end
 
 Rolator.run
